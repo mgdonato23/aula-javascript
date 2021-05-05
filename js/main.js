be814a96ca9812015ff8f1de9345621c0b586e0a
mgdonato23 <mg.donato23@gmail.com>
2021-05-05 12:48:23 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,108 @@
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://google.com.br");
+    //abre em outra página
+    window.location.href = "https://google.com.br";
+    //abre na página atual
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+}*/
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5 , 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));*/
+
+
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());*/
+
+
+
+
+/*var count;
+for( count = 0; count <= 5; count++){
+    alert(count);
+}*/
+
+
+
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}*/
+
+
+
+
+/*var idade = prompt("Qual sua idade? ");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    ("menor de idade");
+}*/
+
+
+
+
+/*var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);*/
+
+
+
+
+/*var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+//esta função adciona mais um elemento
+console.log(lista);
+lista.pop();
+//esta função retira o ultimo elemento
+console.log(lista);
+console.log(lista[0]);
+//esta função lista a posição selecionada
+console.log(lista.reverse());
+//muda a posição da lista
+console.log(lista.toString());
+//transforma o array em uma string
+console.log(lista.join(" / "));
+//esta função define o que divide as palavras*/
+
+
+
+
+/*var nome = "Matheus Donato";
+var idade = 19;
+var idade2 = 10;
+var frase = "Japão é o MELHOR time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão", "Brasil"));
+//replace substitui o primeiro valor passado pelo segundo
+console.log(frase.toUpperCase());
+//toUpperCase deixa o texto todo em caixa alta
+console.log(frase.toLowerCase());
+//toLowerCase deixa o texto todo em caixa baixa*/
